1da1a7c08d1ffc291a86800a338fa9be0301fb4b
Orien Madgwick <vcs@orien.io>
2026-07-15 10:39:43 +1000

    chore(deps): upgrade uuid to v14
    uuid v12+ is ESM-only and uses the exports field / Web Crypto API.
    Adapt the Jest 24 harness to resolve and transform it:
    - map bare uuid import to its entry file (Jest 24 predates exports)
    - allow Babel to transpile uuid's ESM
    - polyfill Web Crypto in the jsdom test setup

--- jest/setup.ts
+++ jest/setup.ts
@@ -2,5 +2,11 @@ import raf from './polyfills/raf';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+// jsdom's test environment doesn't expose the Web Crypto API that uuid v14 relies on.
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { webcrypto } = require('crypto');
+// @ts-ignore
+if (!global.crypto) global.crypto = webcrypto;
+
 // @ts-ignore
 configure({ adapter: new Adapter() });
